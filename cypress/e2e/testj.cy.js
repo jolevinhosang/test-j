520cc606d74f1f1884ffbf979e60854b65408342
Jolevin Armando Hosang <jolevinarmando@refactory.id>
2025-07-28 19:17:49 +0700

    test failed test

--- cypress/e2e/testj.cy.js
+++ cypress/e2e/testj.cy.js
@@ -1,6 +1,17 @@
-describe('template spec', () => {
-  it('passes', () => {
+// cypress/e2e/simple_test.cy.js
+
+describe('Simple Cypress Test', () => {
+  it('Visits the example site and checks navigation', () => {
+    // Visit the homepage
     cy.visit('https://example.cypress.io')
 
+    // Click on the "Querying" link in the Commands menu
+    cy.contains('Querying').click()
+
+    // Verify the URL includes '/commands/querying'
+    cy.url().should('include', '/commands/querying')
+
+    // Assert that the page contains the correct heading
+    cy.get('h1').should('contain', 'Querying')
   })
 })
